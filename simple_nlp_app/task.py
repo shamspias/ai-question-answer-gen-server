5b03f1b28e5cff2b28f61c85022e5ecdb8ec67a9
Shamsuddin Ahmed <shamsudinpias0@gmail.com>
2022-12-03 11:18:22 +0600

    feat: answer gen

--- simple_nlp_app/task.py
+++ simple_nlp_app/task.py
@@ -13,7 +13,39 @@ def generate_questions(types, prompt):
 
     response = openai.Completion.create(
         model="text-davinci-003",
-        prompt=" Write \n {}\n questions based on {}\n-".format(types, prompt),
+        prompt="Write \n {}\n questions based on {}\n-".format(types, prompt),
+        temperature=0.7,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    my_text = response['choices'][0]['text'].split("\n")
+
+    new_text = []
+
+    for i, word in enumerate(my_text):
+        if len(word) > 4:
+            if word[0] == '-':
+                new_text.append(word[1:])
+            else:
+                new_text.append(word)
+    context = {
+        'data': new_text
+    }
+    return context
+
+
+@shared_task(name='GenerateAnswers')
+def generate_answers(number, prompt):
+    """
+    Method to generate question answers from OpenAI using GPT3
+    """
+    openai.api_key = settings.OPEN_AI_KEY
+
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt="Write {} answer for \n {}\n-".format(number, prompt),
         temperature=0.7,
         max_tokens=256,
         top_p=1,
