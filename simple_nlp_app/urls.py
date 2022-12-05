e904e8a1926b12e46515f8b5565a394e1c758610
Shamsuddin Ahmed <shamsudinpias0@gmail.com>
2022-12-06 01:41:06 +0600

    feat:Conversational Direction url

--- simple_nlp_app/urls.py
+++ simple_nlp_app/urls.py
@@ -2,9 +2,15 @@ from rest_framework.routers import DefaultRouter
 
 from .views import (
     QuestionGenViewSet,
-    AnswerGenViewSet
+    AnswerGenViewSet,
+    PersonaViewSet,
+    VerticalViewSet,
+    SalesPersonViewSet
 )
 
 simple_app_router = DefaultRouter()
 simple_app_router.register('questions', QuestionGenViewSet)
 simple_app_router.register('answer', AnswerGenViewSet)
+simple_app_router.register('persona', PersonaViewSet)
+simple_app_router.register('vertical', VerticalViewSet)
+simple_app_router.register('sales-person', SalesPersonViewSet)
