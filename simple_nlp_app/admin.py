1c63fc9b1139c885ce6c43e13ba8b8f036dcd975
Shamsuddin Ahmed <shamsudinpias0@gmail.com>
2022-12-06 01:32:34 +0600

    feat:Conversational Direction admin

--- simple_nlp_app/admin.py
+++ simple_nlp_app/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
-from .models import Questions, Answer
+from .models import Questions, Answer, Persona, Vertical, SalesPerson
 
 admin.site.register(Questions)
 admin.site.register(Answer)
+
+
+admin.site.register(Persona)
+admin.site.register(Vertical)
+admin.site.register(SalesPerson)
