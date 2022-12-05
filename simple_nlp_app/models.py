bc93f6fcbf47e922de306a7edb590556de458627
Shamsuddin Ahmed <shamsudinpias0@gmail.com>
2022-12-06 01:31:26 +0600

    feat: add Conversational Direction

--- simple_nlp_app/models.py
+++ simple_nlp_app/models.py
@@ -24,3 +24,40 @@ class Answer(models.Model):
     persona = models.CharField(max_length=250, blank=True, null=True)
     number = models.IntegerField(default=1)
     answer = models.TextField(blank=True, null=True)
+
+
+class ConversationalDirection(models.Model):
+    """
+    To create and set information
+    """
+    pass
+
+
+class Persona(models.Model):
+    """
+    Model for Persona
+    """
+    name = models.CharField(max_length=250, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Vertical(models.Model):
+    """
+    Model for Vertical
+    """
+    name = models.CharField(max_length=250, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class SalesPerson(models.Model):
+    """
+    Model for Vertical
+    """
+    name = models.CharField(max_length=250, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
