e3ba8be4977a5e1df73b9b9140bfcdc0d7536866
Shamsuddin Ahmed <shamsudinpias0@gmail.com>
2022-12-06 16:39:04 +0600

    fix: models admin

--- simple_nlp_app/admin.py
+++ simple_nlp_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Persona, Vertical, SalesPerson
+from .models import Persona, Vertical, SalesPerson, ConversationalDirection
 
 admin.site.register(Persona)
 admin.site.register(Vertical)
 admin.site.register(SalesPerson)
+admin.site.register(ConversationalDirection)
